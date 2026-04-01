534b32b93ea58023e2bda07b90cbe6201dd013eb
Angel Gabriel Retana Rivas <ratanarivas@gmail.com>
2026-04-01 14:23:09 -0600

    add patient-service project structure

---
+++ patient-service/src/test/java/com/retanarivas/patient_service/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retanarivas.patient_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
